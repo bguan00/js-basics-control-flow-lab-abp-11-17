54314f8aca11cac2854e5c41df71c133156fcb02
Bo Guan <bguan00@gmail.com>
2017-11-14 02:35:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+function scuberGreetingForFeet(x){
+  if (x <= 400)
+  {return 'This one is on me!'}
+  else if (x>2000 && x<=2500) {return 'I will gladly take your thirty bucks.'}
+  else if (x>2500){return'No can do.'}
+};
+
+function ternaryCheckCity(x) { return x='NYC'? 'Ok, sounds good.':'No go.'}
