6da8c35f416f23aca03d9624a548ce56a30dc6d6
Bo Guan <bguan00@gmail.com>
2017-11-14 03:27:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,12 @@ function scuberGreetingForFeet(x){
   else if (x>2500){return'No can do.'}
 }
 
-function ternaryCheckCity(x) { return x!='NYC'?'No go.':"Ok, sounds good."}
+function ternaryCheckCity(x) { return x!='NYC'? 'No go.':"Ok, sounds good."}
+
+function switchOnCharmFromTip(x) {
+  switch(x) {case x>4 : return 'Thank you so much.';
+    break;
+    case x<4&& x>3: return  
+    case x<3: return 'bye'
+  x}
+}
