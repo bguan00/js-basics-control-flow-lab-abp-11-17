5e9b350f0c538a9cd20969bd6fc6d81d32f74f7d
Bo Guan <bguan00@gmail.com>
2017-11-14 03:34:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function ternaryCheckCity(x) { return x!='NYC'? 'No go.':"Ok, sounds good."}
 function switchOnCharmFromTip(x) {switch (x) {
 
     case x='generous': return 'Thank you so much.'
-    case x!=: return 'Thank you.'
+    case x!= 'generous': return 'Thank you.'
     case x= 'anything else':  return 'bye';
     ;}
 }
